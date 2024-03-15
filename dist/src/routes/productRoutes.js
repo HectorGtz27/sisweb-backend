ed346c25f3623617f97569e4020e4e13aaec897e
HectorGtz27 <alan.gtz51@gmail.com>
2024-03-14 17:56:52 -0700

    All finished

--- dist/src/routes/productRoutes.js
+++ dist/src/routes/productRoutes.js
@@ -1,49 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// Aqui se importa el router, request y response de express para
-// poder crear las rutas de la aplicacion
 const express_1 = require("express");
-// Aqui se crea una instancia de router de express, router devu
-// elve un objeto de tipo router
+const productController_1 = require("../controllers/productController");
 const productRouter = (0, express_1.Router)();
-// Aqui se utiliza el metodo get para obtener la lista de productos
-productRouter.get("/", (req, res) => {
-    res.send("Get a list of products");
-});
-// Aqui se utiliza el get para obtener un solo producto con un id
-productRouter.get("/:id", (req, res) => {
-    // Manda un mensaje con el id del producto
-    let id = req.params.id;
-    res.send(`Get the product ${id}`);
-});
-// Crear un nuevo producto, el enrutador utiliza el metodo post
-// para crear un nuevo producto
-// Cual es la ruta de post para crear un nuevo producto: /product
-productRouter.post("/", (req, res) => {
-    // Se obtiene el id, titulo y precio del producto
-    // req.boy es el cuerpo de la peticion que se envia desde el cliente
-    // y se obtiene el id, titulo y precio del producto
-    let id = req.body.id;
-    let title = req.body.title;
-    let price = req.body.price;
-    // Manda un mensaje con el id, titulo y precio del producto
-    res.send(`Create a new product with ID: ${id}, Title: ${title} and Price: ${price}`);
-});
-// Actualizar un producto, el enrutador utiliza el metodo patch para actualizar un producto
-productRouter.patch("/:id", (req, res) => {
-    res.send(`Update the product ${req.params.id} with title ${req.body.title}, and price of ${req.body.price}`);
-});
-// Ahora vamos a eleminar un producto, el enrutador utiliza el metodo delete
-// para eliminar un producto. No se pone /:id porque se va a eliminar el producto
-// y no se quiere que el usuario pueda ver el id del producto que se va a eliminar
-// por eso es que en .get se pone /:id para que el usuario pueda ver el id del producto
-productRouter.delete("/", (req, res) => {
-    let id = req.body.id;
-    res.send(`Deleting the product ${id}`);
-});
-// Se exporta el enrutador para poder utilizarlo en otros archivos
-// de la aplicacion
+productRouter.get("/", productController_1.getAllProducts);
+productRouter.get("/:id", productController_1.getProductById);
+productRouter.post("/", productController_1.createProduct);
+productRouter.patch("/:id", productController_1.modifyProduct);
+productRouter.delete("/", productController_1.deleteProduct);
 exports.default = productRouter;
-// La diferencia entre req.body y req.params es que req.body se utiliza para obtener
-// los datos que se envian desde el cliente, mientras que req.params se utiliza para
-// obtener los parametros que se envian desde el cliente.
